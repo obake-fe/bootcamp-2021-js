7db0ced1a6168f2c462442524fdfd3f7ea30b2af
obake-fe <daichismash@gmail.com>
2021-03-28 17:39:58 +0900

    feat: comment shell script

--- client/run.bash
+++ client/run.bash
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+#エラーが起こった場合、即終了する
+#https://inokara.hateblo.jp/entry/2020/06/27/084149
+#https://www.atmarkit.co.jp/ait/articles/1805/18/news038.html
 set -eu
 
+#sleep - 指定した時間だけ処理を遅延 - Linuxコマンド
+#https://webkaru.net/linux/sleep-command/
 sleep 3
 
+#ウェブサーバーの起動
+#https://www.php.net/manual/ja/features.commandline.webserver.php
 php -S localhost:1234
--- run.bash
+++ run.bash
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+# && はAND制御演算子「コマンド1が成功したらコマンド2を実行する」
+#https://eng-entrance.com/linux-basic-and
+#コマンドをバックグランドジョブとして使うためのコマンド
+#https://eng-entrance.com/linux-basic-background
 (cd client && ./run.bash) &
 (cd server && npm run start) &
 
+# 現在のシェルが実行している全てのジョブが終了するのを待つ
+# https://www.atmarkit.co.jp/ait/articles/1906/28/news025.html
 wait
